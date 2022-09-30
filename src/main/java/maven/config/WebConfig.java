a6f17d7717ad54536f125cd404219beedfa4ae19
b0b1n <sohaibskious@gmail.com>
2022-09-30 12:52:07 +0200

    Setting up the dispatcherServlet

---
+++ src/main/java/maven/config/WebConfig.java
@@ -0,0 +1,11 @@
+package maven.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "maven")
+public class WebConfig {
+}
